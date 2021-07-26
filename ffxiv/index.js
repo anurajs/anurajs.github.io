bf4ca8246a2743a14deea652c8df0fb8574c114b
Anuraj Tapidas <atapidas@gmail.com>
2021-07-26 18:35:40 +0100

    added new constructor to date objects

--- ffxiv/index.js
+++ ffxiv/index.js
@@ -1,6 +1,7 @@
-let original = Date("2021/07/16");
-let now = Date();
+let original = new Date("2021-07-16");
+let now = new Date();
 let difference = now - original;
 let day = 1000 * 60 * 60 * 24;
 let days = Math.floor(difference / day);
+console.log(original, now);
 document.getElementById("time").innerHTML = `Days since last dungeon: ${days}`;
