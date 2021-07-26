010e2abb04932aa316d7695c0ddf48d472881c44
Anuraj Tapidas <atapidas@gmail.com>
2021-07-26 18:29:53 +0100

    added ffxiv branch

---
+++ ffxiv/index.js
@@ -0,0 +1,6 @@
+let original = Date("2021/07/16");
+let now = Date();
+let difference = now - original;
+let day = 1000 * 60 * 60 * 24;
+let days = Math.floor(difference / day);
+document.getElementById("time").innerHTML = `Days since last dungeon: ${days}`;
