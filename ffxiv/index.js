5301b582f4065353dd71e9a2863ef2efd583a27a
Anuraj Tapidas <atapidas@gmail.com>
2021-07-30 20:28:26 +0100

    reading date from json

--- ffxiv/index.js
+++ ffxiv/index.js
@@ -1,9 +1,15 @@
-let original = new Date(
-	"Fri Jul 30 2021 20:22:27 GMT+0100 (British Summer Time)"
-);
-let now = new Date();
-let difference = now - original;
-let day = 1000 * 60 * 60 * 24;
-let days = Math.floor(difference / day);
-document.getElementById("time").innerHTML = `Days since last dungeon: ${days}`;
-document.title = `${days} Days since last Dungeon`;
+fetch("https://anurajs.github.io/ffxiv/data.json")
+	.then((response) => {
+		let original = new Date(response.json.date);
+		let now = new Date();
+		let difference = now - original;
+		let day = 1000 * 60 * 60 * 24;
+		let days = Math.floor(difference / day);
+		document.getElementById(
+			"time"
+		).innerHTML = `Days since last dungeon: ${days}`;
+		document.title = `${days} Days since last Dungeon`;
+	})
+	.catch((e) => {
+		console.log(e);
+	});
