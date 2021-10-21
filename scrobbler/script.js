0cd6dc79ef6bf5cdb2e749e4933e7305f311a8f1
Anuraj Tapidas <atapidas@gmail.com>
2021-10-21 18:41:08 +0100

    added web scrobbler

---
+++ scrobbler/script.js
@@ -0,0 +1,67 @@
+let songText = document.getElementById("song");
+let urlParams = new URLSearchParams(window.location.search);
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow"
+};
+
+let lastSong = null;
+
+fetch;
+
+const getSong = (requestOptions) => {
+  const url = "http://ws.audioscrobbler.com/2.0/";
+  return new Promise((resolve, reject) => {
+    fetch(
+      `http://ws.audioscrobbler.com/2.0/?api_key=${urlParams.get(
+        "key"
+      )}&user=${urlParams.get(
+        "user"
+      )}&format=json&method=user.getrecenttracks&limit=1`,
+      requestOptions
+    )
+      .then(async (resp) => {
+        let details = await resp.json();
+        details = details["recenttracks"]["track"][0];
+        if (details["@attr"] && details["@attr"].nowplaying) {
+          let song = {
+            name: details["name"],
+            artist: details["artist"]["#text"]
+          };
+          resolve(song);
+        } else {
+          reject("Not listening to anything");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.data.error == 6) {
+          reject("Check your api key or username in the .env file");
+        } else {
+          reject(err.response.data.message);
+        }
+      });
+  });
+};
+
+let lastValid = true;
+
+setInterval(() => {
+  getSong(requestOptions)
+    .then((song) => {
+      if (JSON.stringify(song) == JSON.stringify(lastSong)) {
+        return;
+      }
+      lastSong = song;
+      console.log(song);
+      songText.innerHTML = `<< ${song.name} | ${song.artist} >>  `;
+      lastValid = true;
+    })
+    .catch((err) => {
+      if (lastValid) {
+        songText.innerHTML = err;
+        lastValid = false;
+      }
+    });
+}, 4000);
